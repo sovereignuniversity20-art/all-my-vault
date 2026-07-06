c3b00625a2aa0a656e9b0b4eebc21e6b8e908553
Hero <sovereign.university20@gmail.com>
2026-07-06 10:05:39 -0500

    react with vite and react router installed

---
+++ all-my-vault/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect, useRef, useParams } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App

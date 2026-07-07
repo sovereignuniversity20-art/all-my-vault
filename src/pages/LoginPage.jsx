edbedf7dd517c1d0c7f82801f173813f47e7e865
Hero <sovereign.university20@gmail.com>
2026-07-07 18:23:02 -0500

    finalize login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,17 +4,17 @@ const LoginPage = (props) => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [errors, setErrors] = useState({email:'', password:''}); 
+    const [errors, setErrors] = useState({email:'', password:'', name:'', confirmPassword:''}); 
     const [currentStatus, setCurrentStatus] = useState('login');
     const [confirmPassword, setConfirmPassword] = useState ('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setErrors({email:'', password:''});
+        setErrors({email:'', password:'', confirmPassword:'', name:''});
 
 
     if (!/^[a-zA-Z0-9.! # \$ % & ' * + - / = ? ^ _  { | } ~.]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(email)) {
-        setErrors({...errors, email:'Email must use lowercase letters, uppercase letters, and special characters (! @ # $ %) only'})
+        setErrors({...errors, email:'Please enter a valid email address'})
         console.log('Email must use lowercase letters, uppercase letters, and special characters (! @ # $ %) only');
     } else if ( password.length < 8) {
         setErrors({...errors, password:'Password must be at least 8 characters long'})
@@ -31,7 +31,11 @@ const LoginPage = (props) => {
     } else if (!/[!@#$%]/.test(password)) {
         setErrors({...errors, password:'Password must contain at least one special character, ! @ # $ % .'})
         console.log('Password must contain at least one special character.');
-    }   else {
+    } else if (currentStatus === 'signup' && !name) {
+        setErrors({...errors, name:"Name field can not be left empty"}); 
+    } else if (currentStatus === 'signup' &&  password !== confirmPassword) {
+        setErrors({...errors, confirmPassword:"Passwords do not match, please re-enter."});
+    } else {
         props.onLogin(true);
         console.log('Password is valid!');
     }
@@ -58,7 +62,25 @@ const LoginPage = (props) => {
     />
     {errors.password && <span>{errors.password}</span>}
     </label>
-    <button type="submit">Open Vault</button>
+    {currentStatus === 'signup' && (
+        <label>
+        Confirm Password:
+        <input type="password" value={confirmPassword}
+        onChange={(e) => setConfirmPassword(e.target.value)}  />
+        {errors.confirmPassword && <span>{errors.confirmPassword}</span>}
+        </label>
+    )} 
+     {currentStatus === 'signup' && (
+        <label>
+        First and Last Name:
+        <input type="text" value={name}
+        onChange={(e) => setName(e.target.value)}  />
+        {errors.name && <span>{errors.name}</span>}
+        </label>
+    )} 
+    <button type="submit">{currentStatus === 'signup' ? "Create Vault" : "Open Vault"}</button>
+    <button type="button" onClick={() => currentStatus === 'login' ? setCurrentStatus('signup') : setCurrentStatus ('login')}>{currentStatus === 'signup' ? "Already have an account? Log in here" : "Sign up for an account"}</button>
+   
     </form>
     </div>
 );

f71fc62ee3227c4b773eae4af081afedd9fd7977
Hero <sovereign.university20@gmail.com>
2026-07-08 15:12:53 -0500

    sampleData added

---
+++ src/data/sampleData.js
@@ -0,0 +1,50 @@
+const sampleItems = [
+{
+  id:1,
+  title: "research data one",
+  type: "pdf",
+  tags: ["research", "urban farming", "agriculture"],
+  dateAdded: "7.7.26",
+  thumbnail: 0
+},
+{
+  id:2,
+  title: "Eifel Tower visit",
+  type: "jpg",
+  tags: ["Europe", "travel", "Paris", "study-abroad"],
+  dateAdded: "7.7.26",
+  thumbnail: 0
+}, 
+{
+  id:3,
+  title: "Experiential learning dissertation",
+  type: "pdf",
+  tags: ["doctorate", "study", "education", "experiential learning"],
+  dateAdded: "7.7.26",
+  thumbnail: 0 
+},
+{
+  id:4,
+  title: "Sovereign University logo",
+  type: "png",
+  tags: ["marketing", "business", "logo"],
+  dateAdded: "7.7.26",
+  thumbnail: 0  
+},
+{
+  id:5,
+  title: "Software dev study plan",
+  type: "pdf",
+  tags: ["tech", "professional", "study tool", "experiential learning"],
+  dateAdded: "7.7.26",
+  thumbnail: 0    
+},
+{
+  id:6,
+  title: "website background from midjourney",
+  type: "png",
+  tags: ["ai generatiom", "marketing", "web design", "tech"],
+  dateAdded: "7.7.26",
+  thumbnail: 0
+}
+]
